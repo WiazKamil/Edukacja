4c9262d27fbab57fc5a8cd5f0f177e2019441efd
WiazKamil <89968947+WiazKamil@users.noreply.github.com>
2021-09-09 10:44:13 +0200

    Add files via upload

---
+++ zsl/lekcja 2b.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<body style="background-color:red">
+
+<div style="background-color:gray;border:2px black solid;border-radius:10px;text-align:center;">
+<?php
+echo "<br>";
+echo PHP_VERSION;
+echo "<br><br>.";
+// this is a comment
+?>
+</div>
+</body>
+</html>
